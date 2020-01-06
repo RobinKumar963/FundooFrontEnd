713d9a4367c62657f483cf36f85fb9be0460e75a
RobinKumar963 <robbhood123@gmail.com>
2020-01-06 15:42:30 +0530

    [RobinKumar] Refactor package.json

--- src/app/Components/fundoo-admin-login/fundoo-admin-login.component.ts
+++ src/app/Components/fundoo-admin-login/fundoo-admin-login.component.ts
@@ -12,7 +12,7 @@ export class FundooAdminLoginComponent implements OnInit {
 
   constructor(private service:AdminAccountService,private router: Router) { }
   
-  adminLoginModel = new AdminLogin('','');
+  adminLoginModel = new AdminLogin('u','u');
   ngOnInit() {
   }
   
